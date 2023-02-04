546714dd45836428a4662fd6d34badecc74bc594
SimoTaf <stafifet@inttic.dz>
2023-02-04 12:23:41 +0100

    This is my Master_Degree project.

--- Library/entrypoint.sh
+++ Library/entrypoint.sh
@@ -1,44 +1,54 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
+# wait for mysql service to start
+echo "Waiting for mysql service to start..."
+while ! mysqladmin ping -h"db" --silent; do
+    sleep 1
+done
+echo "MySQL service started."
+
+# create the database and user
+echo "Creating database and user..."
+mysql -h db -u root -prootpassword -e "CREATE DATABASE tafifet; CREATE USER 'sidotaf'@'%' IDENTIFIED BY '1234'; GRANT ALL PRIVILEGES ON tafifet.* TO 'sidotaf'@'%'; FLUSH PRIVILEGES;"
+echo "Database and user created."
+
+if [ "$DATABASE" = "mysql" ]
 then
-    echo "Waiting for postgres..."
+    echo "Waiting for MySQL..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
 
-    echo "PostgreSQL started"
+    echo "MySQL started"
+else
+    echo "Unknown database, using SQLite by default"
 fi
 
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
+
 script="
 from library.models import Language , Genre
 
 Languages=['English','Arabic','French','Spanish','Russian']
 
-Genres=['Fantasy','Adventure','Romance','Contemporary','Dystopian','Mystery','Horror','Thriller','Paranormal','Historical fiction','Science Fiction','Memoir','Cooking','Self-help','Art','Development','Motivational','Health','History','Travel','Humor']
+Genres=['Fantasy','Adventure','Romance','Contemporary','Dystopian','Mystery','Horror','Thriller','Paranormal','Historical']
 
-for i in Languages:
-    l=Language(name=i)
+for lang in Languages:
+    l=Language(name=lang)
     l.save()
 
-
-#print('the languages have been added successfully')
-
-
-
-for i in Genres:
-    l=Genre(name=i)
-    l.save()
-
-#print('the themes have been added successfully')
+for genre in Genres:
+    g=Genre(name=genre)
+    g.save()
 "
+
+echo "$script" | python manage.py shell
+
 printf "$script" | python manage.py shell
-python manage.py createsuperuser_if_none_exists --user=admin --password=Admin1234
-
-
+python manage.py createsuperuser_if_none_exists --user=sido --password=sido
 
 exec "$@"
+
--- Library/library_management/settings.py
+++ Library/library_management/settings.py
@@ -79,22 +79,16 @@ WSGI_APPLICATION = 'library_management.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
-    
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbname',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
-
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
